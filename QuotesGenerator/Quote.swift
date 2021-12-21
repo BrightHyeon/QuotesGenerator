a68073ac9b215d46d333e2f00c85ce1c4b665f3a
BrightHyeon <swiftist9891@gmail.com>
2021-12-21 18:25:43 +0900

    first commit

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by HyeonSoo Kim on 2021/12/13.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}

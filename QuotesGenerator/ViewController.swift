c21722b166d6b2c18f8c354897e444623bb267f6
BrightHyeon <swiftist9891@gmail.com>
2021-12-24 18:23:04 +0900

    DOCS: Change constant quotes's value
    I changed the list value of constant quotes in ViewController

--- QuotesGenerator/ViewController.swift
+++ QuotesGenerator/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var quoteLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     let quotes = [
-        Quote(contents: "죽음을 두려워하는 나머지 삶을 시작조차 못하는 사람이 많다.", name: "벤다이크"),
-        Quote(contents: "나는 나 자신을 빼 놓고는 모두 안다.", name: "비용"),
-        Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌"),
-        Quote(contents: "분노는 바보들의 가슴속에서만 살아간다.", name: "아인슈타인"),
-        Quote(contents: "몇 번이라도 좋다! 이 끔찍한 생이여...다시!", name: "니체")
+        Quote(contents: "너가 진정 하고 싶은 일을 해라.", name: "어머니"),
+        Quote(contents: "25살이 뭐가 늦은 나이냐. 30안됐으면 다 어린거다.", name: "아버지"),
+        Quote(contents: "옆에서 응원할게! 얼마가 걸리든 해보자.", name: "여자친구"),
+        Quote(contents: "당신이 되고 싶었던 어떤 존재가 되기에는 지금도 결코 늦지 않았다.", name: "조지 엘리엇"),
     ]
     
     override func viewDidLoad() {
@@ -25,7 +24,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tabQuoteMaker(_ sender: Any) {
-        let random = Int(arc4random_uniform(5))
+        let random = Int(arc4random_uniform(4))
         let quote = quotes[random]
         self.quoteLabel.text = quote.contents
         self.nameLabel.text = quote.name
